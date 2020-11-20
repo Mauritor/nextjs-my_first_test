281852da5e89a28f16662dfcecfd310c0a9e51be
Mauro <mrtelefonia@gmail.com>
2020-11-20 20:40:26 -0300

    agregado de getInitialProps en la page Test

--- pages/test.js
+++ pages/test.js
@@ -12,7 +12,8 @@ import React from 'react'
     }
 }*/
 
-const test = () => {
+const test = ({data}) => {
+    //console.log(data);
     return (
         <>
             <MyHead />
@@ -21,12 +22,22 @@ const test = () => {
                 <div className="row">
                     <div className="col-sm-12">
                         <h1 className="text-primary my-5">Test</h1>
-                        
+                        {
+                             data.map(user => (
+                             <p key={user.id}>{user.username}</p>
+                             ))
+                         }
                     </div>
                 </div>
             </div>
         </>
     )
 }
+
+test.getInitialProps = async (ctx) => {
+    const res = await fetch('http://jsonplaceholder.typicode.com/users')
+    const data = await res.json();
+    return {data}
+ }
 
 export default test
