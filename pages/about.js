97123edd4da0ab649b31c316abb7650c2edb4f0f
Mauro <mrtelefonia@gmail.com>
2020-11-21 12:54:36 -0300

    agregado de imagen en about

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout';
 import MyHead from '../components/Myhead';
 import Nav from '../components/Nav';
+//import Image from 'next/image'
 
 const about = () => {
     return (
@@ -8,7 +9,27 @@ const about = () => {
             <MyHead />
             <Nav statusAbout={'text-primary'} />
             <Layout>
-                <h1 className="text-primary my-5">About us</h1>
+                <div className="d-flex justify-content-around">
+                    <h1 className="text-primary">About us</h1>
+                    <img
+                        src="https://cdn.pixabay.com/photo/2016/03/09/09/22/workplace-1245776_960_720.jpg" alt="nosotros trabajando"
+                        width={400}
+                        className="mt-5"
+                    />
+                </div>
+                <div className="text-center mt-5">
+                    <h5 className="text-dark">We are Working</h5>
+                </div>
+                <style jsx>
+                    {`
+                    h1{
+                        margin-top: 7rem;
+                    }
+                    img{
+                        border-radius: 8px;
+                    }
+                    `}
+                </style>
             </Layout>
         </>
     )
