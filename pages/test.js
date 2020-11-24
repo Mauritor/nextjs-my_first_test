621aff864307bc57437a03cb91911154cf446895
Mauro <mrtelefonia@gmail.com>
2020-11-24 09:17:22 -0300

    page test para cloudinary

--- pages/test.js
+++ pages/test.js
@@ -1,36 +1,23 @@
 import MyHead from '../components/Myhead';
 import Nav from '../components/Nav';
 import Layout from '../components/Layout'
-import Link from 'next/link';
 
 
 
-const test = ({ data }) => {
-    console.log(data);
+const test = () => {
+    
     return (
         <>
             <MyHead />
             <Nav statusTest={'text-primary'} />
             <Layout >
-                <h1 className="text-primary my-5">Test</h1>
-                {
-                    data.map(item => (
-                        <p key={item.id} >
-                            {item.username}
-                        </p>
-
-                    ))
-                }
+                <h1 className="text-primary my-5">Test Cloudinary</h1>
+                
             </Layout>
         </>
 
     )
 }
 
-test.getInitialProps = async (ctx) => {
-    const res = await fetch('http://jsonplaceholder.typicode.com/users')
-    const data = await res.json();
-    return { data }
-}
 
 export default test
